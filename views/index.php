9be3e4a5476849f4547a9ec1b425dd85fd2f6f53
Charlie Rudenstål <charlie4@gmail.com>
2012-06-15 23:12:06 +0300

    Added versioning of CSS files

--- views/index.php
+++ views/index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <link href='http://fonts.googleapis.com/css?family=Lato:100|Quicksand:300|Josefin+Sans:100|Exo:100|Raleway:100' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="/css/default.css">
+    <link rel="stylesheet" type="text/css" href="/css/default.css?1">
     <link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript" src="//api.filepicker.io/v0/filepicker.js"></script>

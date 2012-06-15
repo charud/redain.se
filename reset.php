8039ce94cbee3a62f90735c21cbd57dfa590d11f
Charlie Rudenstål <charlie4@gmail.com>
2012-06-15 19:08:55 +0200

    Added sample response

--- reset.php
+++ reset.php
@@ -17,25 +17,30 @@ R::wipe("request");
 $request = R::dispense("request");
 $request->text = "Vilken soffa ska jag ha till mitt hem?";
 $request->image = "http://julgran.erikolsson.se/upload/Sotmandeln_vardagsrum.JPG";
-$id = R::store($request);
+R::store($request);
 
 $request = R::dispense("request");
 $request->text = "Behöver en ny matta, några tips?";
 $request->image = "http://www.vitdesign.se/sandra/uploaded_images/vardagsrum_1-764110.jpg";
-$id = R::store($request);
+R::store($request);
 
 $request = R::dispense("request");
 $request->text = "Vi funderar på att kakla om. Vad tror ni om ett ljust?";
 $request->image = "http://2.bp.blogspot.com/_uAzLaJnCUas/SU6VpRfwtoI/AAAAAAAAAN0/g5GJSqqGHH8/s400/k%C3%B6k_myresj%C3%B6_AccentKvadratCap.jpg";
-$id = R::store($request);
+R::store($request);
 
+    $response = R::dispense("response");
+    $response->text = "Tror att det kan bli för vitt. Testa ett anticitgrått matt kakel. Kan bli riktigt stiligt!";
+    $response->request = $request;
+    R::store($response);
+    
 $request = R::dispense("request");
 $request->text = "En passande hylla ovanför min soffa";
 $request->image = "http://designinsight.se/wp-content/uploads/2011/03/vardagsrum.jpg";
-$id = R::store($request);
+R::store($request);
 
 $request = R::dispense("request");
 $request->text = "Vill ha en passande stol här. Vilken tycker ni skulle fungera bäst?";
 $request->image = "http://cdn.freshome.com/wp-content/uploads/2008/04/living-room-modern.jpg";
-$id = R::store($request);
+R::store($request);
 
